5f43323c5ee8a03221d98efcfa93f283e78b043d
Dmytro Asparian <dima.supckik2004@gmail.com>
2024-12-07 13:43:09 +0200

    MongoDB connect

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use warp::Filter;
+use dotenvy::dotenv;
+use mongodb::{options::ClientOptions, Client};
+use std::env;
 
 #[tokio::main]
-async fn main() {
-    // Для перевірки
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+async fn main() -> mongodb::error::Result<()> {
+    // Завантажуємо змінні середовища з .env файлу
+    dotenv().ok();
 
-    // Запус сервера
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    // Отримуємо URI з середовища
+    let uri = env::var("MONGODB_URI").expect("Змінна середовища MONGODB_URI не встановлена");
+
+    // Налаштування клієнта MongoDB
+    let options = ClientOptions::parse(&uri).await?;
+    let client = Client::with_options(options)?;
+
+    // Перевірка підключення
+    println!("Підключення до MongoDB успішне!");
+
+    // Отримання доступу до бази даних
+    let db = client.database("chat_app");
+    println!("Використовується база даних: {}", db.name());
+
+    Ok(())
 }
